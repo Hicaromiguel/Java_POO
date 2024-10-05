e5f3559d4eb04579ae3e78459cd4751f23f020ef
HI <153924670+Hicaromiguel@users.noreply.github.com>
2024-10-05 19:50:52 -0300

    Update Main.java

--- TESTE/src/Main.java
+++ TESTE/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static  void main (String[]args){
-        Cachorro mycachorro = new Cachorro();
+       achorro mycachorro = new Cachorro();
+        Gato mygato = new Gato();
+        mycachorro.VerStatus();
+        mygato.EngolirChoro();
+        mygato.VerStatus();
     }
 }
